2db04a751a070742dc5a148d438c9ed3176f73b5
crispysodium <christinena.316@gmail.com>
2020-05-01 03:21:43 -0700

    changed STATIC_CACHE to PRE_CACHE & removed some code from fetch

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,18 +2,16 @@ const FILES_TO_CACHE = [
     "/",
     "/index.html",
     "/styles.css",
-    "/dist/bundle.js",
-    "/dist/manifest.e32445d81bcf4e5cfbf80ffbddd842d8.json",
-
+    "/dist/bundle.js"
 ];
 
-const STATIC_CACHE = "static-cache-v1";
-const RUNTIME_CACHE = "runtime-cache";
+const PRE_CACHE = "precache-v1";
+const RUNTIME_CACHE = "runtime";
 
 self.addEventListener("install", event => {
     event.waitUntil(
       caches
-        .open(STATIC_CACHE)
+        .open(PRE_CACHE)
         .then(cache => cache.addAll(FILES_TO_CACHE))
         .then(() => self.skipWaiting())
     );
@@ -21,53 +19,24 @@ self.addEventListener("install", event => {
 
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", event => {
-    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    const currentCaches = [PRE_CACHE, RUNTIME_CACHE];
     event.waitUntil(
         caches
         .keys()
         .then(cacheNames => {
             // return array of cache names that are old to delete
-            return cacheNames.filter(
-            cacheName => !currentCaches.includes(cacheName)
-            );
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
         })
         .then(cachesToDelete => {
-            return Promise.all(
-            cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            })
-            );
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                    return caches.delete(cacheToDelete);
+            }));
         })
         .then(() => self.clients.claim())
     );
 });
 
 self.addEventListener("fetch", event => {
-    // non GET requests are not cached and requests to other origins are not cached
-    if (
-        event.request.method !== "GET" ||
-        !event.request.url.startsWith(self.location.origin)
-    ) {
-        event.respondWith(fetch(event.request));
-        return;
-    }
-
-    // handle runtime GET requests for data from /api routes
-    if (event.request.url.includes("/api/images")) {
-        // make network request and fallback to cache if network request fails (offline)
-        event.respondWith(
-            caches.open(RUNTIME_CACHE).then(cache => {
-                return fetch(event.request)
-                    .then(response => {
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                    .catch(() => caches.match(event.request));
-            })
-        );
-        return;
-    }
-    
     if(event.request.url.startsWith(self.location.origin)) {
         // use cache first for all other requests for performance
         event.respondWith(
